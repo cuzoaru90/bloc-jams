f66763d74b95e7f48fb8a0eaaa8a18947bb8fc84
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-11-05 08:14:02 -0600

    Updated events assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,20 @@
        var pointsArray = document.getElementsByClassName('point');
 
-           var animatePoints = function(points) {
-           
-           var revealPoint = function revealPoint(index) {
-             points[index].style.opacity = 1;
-             points[index].style.transform = "scaleX(1) translateY(0)";
-             points[index].style.msTransform = "scaleX(1) translateY(0)";
-             points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+           // Function now accepts point param instead of index param, style methods called on point instead of points[index]
+           // Previous code worked properly, but this solution code is better written
+           var revealPoint = function revealPoint(point) {
+             point.style.opacity = 1;
+             point.style.transform = "scaleX(1) translateY(0)";
+             point.style.msTransform = "scaleX(1) translateY(0)";
+             point.style.WebkitTransform = "scaleX(1) translateY(0)";
            };
+
+           var animatePoints = function(points) { 
 
            // for (var i = 0; i < points.length; i++){
            //   revealPoint(i);
            // }
-
+           
            forEach(points, revealPoint);
 
         }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,8 +1,9 @@
 
+        // revealPoints uses point param instead of index param, so callback(i) has changed to callback(elements[i])
         function forEach(elements, callback){  
 
             for (var i = 0; i < elements.length; i++){
-              callback(i);
+              callback(elements[i]);
             }
 
         }
