c6ea2ff7bf0ac8d41c53d86ea9579dab8aa19f1a
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-11-14 12:03:52 -0600

    Successfully rendered styles and javascript for Bloc Jams

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 
-app.use(express.static(__dirname + '/scripts/landing.js'));
-app.use(express.static(__dirname + '/styles/landing.css'));
+app.use(express.static(__dirname + "/"));
 
-app.get('/',function(req,res){
+app.get('/', function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.get('/collection.html',function(req,res){
   res.sendFile(path.join(__dirname+'/collection.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.get('/album.html',function(req,res){
   res.sendFile(path.join(__dirname+'/album.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.listen(3000);
