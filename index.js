bf9934b50bc7a29a52bcf8e2188f8b39a544da4a
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-11-14 13:08:11 -0600

    Changed port information

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + "/"));
 
 app.get('/', function(req,res){
@@ -16,6 +18,6 @@ app.get('/album.html',function(req,res){
   res.sendFile(path.join(__dirname+'/album.html'));
 });
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
